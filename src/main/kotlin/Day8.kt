78fb435b4355a4ee9216ec1902bd9a086c86b02e
Helge Wessels <helge.wessels@rewe-digital.com>
2020-12-08 19:48:16 +0100

    day 8

---
+++ src/main/kotlin/Day8.kt
@@ -0,0 +1,62 @@
+class Day8 {
+
+    fun part1(input: String): Int =
+            execute(readInstructions(input)).accumulator
+
+    fun part2(input: String): Int =
+            execute(findRepairedInstructions(readInstructions(input)) ?: emptyList()).accumulator
+
+    fun readInstructions(input: String): List<Pair<String, Int>> =
+            input.trimIndent()
+                    .split("\n")
+                    .map { line: String ->
+                        val instruction = line.split(" ")
+                        Pair(instruction[0], instruction[1].toInt())
+                    }
+
+    fun execute(instructions: List<Pair<String, Int>>): State {
+        var state = State()
+        do {
+            state = executeInstruction(instructions[state.index], state)
+        } while (!state.isInfiniteLoop() && state.index in (instructions.indices))
+        return state
+    }
+
+    fun executeInstruction(instruction: Pair<String, Int>, state: State): State {
+        return when (instruction.first) {
+            "nop" -> state.next()
+            "acc" -> state.accumulate(instruction.second)
+            "jmp" -> state.jump(instruction.second)
+            else -> state
+        }
+    }
+
+    private fun findRepairedInstructions(instructions: List<Pair<String, Int>>): List<Pair<String, Int>>? =
+            instructions
+                    .mapIndexedNotNull { index, instruction -> if (instruction.isRepairable()) index else null }
+                    .map { indexToRepair -> instructions.mapIndexed { index, instruction -> if (index == indexToRepair) instruction.repair() else instruction } }
+                    .firstOrNull { repairedInstructions -> !execute(repairedInstructions).isInfiniteLoop() }
+
+    private fun Pair<String, Int>.isRepairable() = first in REPAIR_MAP.keys
+
+    private fun Pair<String, Int>.repair() = Pair(REPAIR_MAP.getOrDefault(first, first), second)
+
+    companion object {
+        val REPAIR_MAP = mapOf(
+                "nop" to "jmp",
+                "jmp" to "nop"
+        )
+    }
+}
+
+data class State(
+        val index: Int = 0,
+        val executed: List<Int> = emptyList(),
+        val accumulator: Int = 0
+) {
+    fun next() = State(index = index + 1, executed = executed + index, accumulator = accumulator)
+    fun accumulate(value: Int) = State(index = index + 1, executed = executed + index, accumulator = accumulator + value)
+    fun jump(offset: Int) = State(index = index + offset, executed = executed + index, accumulator = accumulator)
+
+    fun isInfiniteLoop(): Boolean = index in executed
+}
---
+++ src/test/groovy/Day8Spec.groovy
@@ -0,0 +1,105 @@
+import kotlin.Pair
+import spock.lang.Specification
+import spock.lang.Subject
+import spock.lang.Unroll
+
+class Day8Spec extends Specification {
+
+    def input = new File(getClass().getResource('/input/day8.txt').toURI()).getText()
+
+    @Subject
+    Day8 day8 = new Day8()
+
+    def part1() {
+        when:
+        def result = day8.part1(input)
+
+        then:
+        println("Part 1: $result")
+        result == 2014
+    }
+
+    def part2() {
+        when:
+        def result = day8.part2(input)
+
+        then:
+        println("Part 1: $result")
+        result == 2251
+    }
+
+    def readInstructions() {
+        given:
+        def input = '''
+            nop +0
+            acc +1
+            jmp +4
+            acc +3
+            jmp -3
+            acc -99
+            acc +1
+            jmp -4
+            acc +6
+        '''
+
+        when:
+        def instructions = day8.readInstructions(input)
+
+        then:
+        instructions.size() == 9
+        instructions[0] == new Pair('nop', 0)
+        instructions[1] == new Pair('acc', 1)
+        instructions[2] == new Pair('jmp', 4)
+        instructions[3] == new Pair('acc', 3)
+        instructions[4] == new Pair('jmp', -3)
+        instructions[5] == new Pair('acc', -99)
+        instructions[6] == new Pair('acc', 1)
+        instructions[7] == new Pair('jmp', -4)
+        instructions[8] == new Pair('acc', 6)
+    }
+
+    def execute() {
+        given:
+        def instructions = [
+                new Pair('nop', 0),
+                new Pair('acc', 1),
+                new Pair('jmp', 4),
+                new Pair('acc', 3),
+                new Pair('jmp', -3),
+                new Pair('acc', -99),
+                new Pair('acc', 1),
+                new Pair('jmp', -4),
+                new Pair('acc', 6)
+        ]
+
+        when:
+        def state = day8.execute(instructions)
+
+        then:
+        state.index == 1
+        state.accumulator == 5
+    }
+
+    @Unroll('executing "#instruction #argument" with index #index and accumulator #accumulator results in index #expectedIndex and accumulator #expectedAccumulator')
+    def executeInstruction(String instruction, int argument, int index, int accumulator, int expectedIndex, int expectedAccumulator) {
+        when:
+        def state = day8.executeInstruction(new Pair<>(instruction, argument), new State(index, [], accumulator))
+
+        then:
+        state.executed == [index]
+        state.index == expectedIndex
+        state.accumulator == expectedAccumulator
+
+        where:
+        instruction | argument | index | accumulator || expectedIndex | expectedAccumulator
+        'nop'       | 0        | 0     | 0           || 1             | 0
+        'nop'       | 0        | 0     | 42          || 1             | 42
+        'acc'       | 1        | 1     | 0           || 2             | 1
+        'acc'       | 1        | 1     | 42          || 2             | 43
+        'jmp'       | 4        | 2     | 0           || 6             | 0
+        'jmp'       | 4        | 2     | 42          || 6             | 42
+        'jmp'       | -1       | 2     | 0           || 1             | 0
+        'jmp'       | -1       | 2     | 42          || 1             | 42
+    }
+
+}
