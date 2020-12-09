87ac988a239bd6c1ff6c95161d002797a449e142
Helge Wessels <helge.wessels@rewe-digital.com>
2020-12-09 09:48:36 +0100

    day 9

---
+++ src/main/kotlin/Day9.kt
@@ -0,0 +1,38 @@
+class Day9 {
+
+    fun part1(input: String, offset: Int) =
+            findFirstInvalid(readNumbers(input), offset)
+
+    fun part2(input: String, offset: Int): Long? {
+        val numbers = readNumbers(input)
+        return findFirstInvalid(numbers, offset)
+                ?.let { findWindowBySum(numbers, it) }
+                ?.let { it.minOrNull()!! + it.maxOrNull()!! }
+    }
+
+    fun readNumbers(input: String) = input.trimIndent()
+            .split("\n")
+            .map { it.toLong() }
+
+    fun findFirstInvalid(numbers: List<Long>, offset: Int): Long? =
+            numbers.indices
+                    .drop(offset)
+                    .firstOrNull { !isValid(it, numbers, offset) }
+                    ?.let { numbers[it] }
+
+    fun findWindowBySum(numbers: List<Long>, sum: Long): List<Long>? =
+            numbers.allWindows().firstOrNull { it.sum() == sum }
+
+    fun isValid(index: Int, numbers: List<Long>, offset: Int): Boolean =
+            numbers.subList(index - offset, index).allPairs().any { chunk -> chunk.sum() == numbers[index] }
+
+    fun List<Long>.allPairs(): List<List<Long>> =
+            indices.toList().dropLast(1).flatMap { first ->
+                indices.toList().drop(first + 1).map { second -> listOf(get(first), get(second)) }
+            }
+
+    fun List<Long>.allWindows(): List<List<Long>> =
+            indices.toList().dropLast(1).flatMap { start ->
+                indices.toList().drop(start + 1).map { end -> subList(start, end + 1) }
+            }
+}
---
+++ src/test/groovy/Day9Spec.groovy
@@ -0,0 +1,145 @@
+import spock.lang.Specification
+import spock.lang.Subject
+
+class Day9Spec extends Specification {
+
+    def input = new File(getClass().getResource('/input/day9.txt').toURI()).getText()
+
+    @Subject
+    Day9 day9 = new Day9()
+
+    def part1() {
+        when:
+        def result = day9.part1(input, 25)
+
+        then:
+        println("Part 1: $result")
+        result == 50047984
+    }
+
+    def part2() {
+        when:
+        def result = day9.part2(input, 25)
+
+        then:
+        println("Part 2: $result")
+        result == 5407707
+    }
+
+    def readNumbers() {
+        given:
+        def input = '''
+            35
+            20
+            15
+        '''
+
+        when:
+        def numbers = day9.readNumbers(input)
+
+        then:
+        numbers == [35, 20, 15] as List<Long>
+    }
+
+    def findFirstInvalid() {
+        given:
+        def offset = 5
+        def numbers = [
+                35L,
+                20L,
+                15L,
+                25L,
+                47L,
+                40L,
+                62L,
+                55L,
+                65L,
+                95L,
+                102L,
+                117L,
+                150L,
+                182L,
+                127L,
+                219L,
+                299L,
+                277L,
+                309L,
+                576L
+        ]
+
+        expect:
+        day9.findFirstInvalid(numbers, offset) == 127
+    }
+
+    def findWindowBySum() {
+        def sum = 127L
+        def numbers = [
+                35L,
+                20L,
+                15L,
+                25L,
+                47L,
+                40L,
+                62L,
+                55L,
+                65L,
+                95L,
+                102L,
+                117L,
+                150L,
+                182L,
+                127L,
+                219L,
+                299L,
+                277L,
+                309L,
+                576L
+        ]
+
+        expect:
+        day9.findWindowBySum(numbers, sum) == [15L, 25L, 47L, 40L]
+    }
+
+    def isValid() {
+        given:
+        def offset = 5
+        def numbers = [
+                35L,
+                20L,
+                15L,
+                25L,
+                47L,
+                40L,
+                62L,
+                55L,
+                65L,
+                95L,
+                102L,
+                117L,
+                150L,
+                182L,
+                127L,
+                219L,
+                299L,
+                277L,
+                309L,
+                576L
+        ]
+
+        expect:
+        [5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19].each {
+            assert day9.isValid(it, numbers, offset)
+        }
+        !day9.isValid(14, numbers, offset)
+    }
+
+    def allPairs() {
+        expect:
+        day9.allPairs([1L, 2L, 3L]) == [[1L, 2L], [1L, 3L], [2L, 3L]]
+    }
+
+    def allWindows() {
+        expect:
+        day9.allWindows([1L, 2L, 3L]) == [[1L, 2L], [1L, 2L, 3L], [2L, 3L]]
+    }
+}
