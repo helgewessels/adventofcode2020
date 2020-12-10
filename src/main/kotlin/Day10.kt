135f288f635542b78c5b82e84f7c8a8c5417f6d6
Helge Wessels <helge.wessels@rewe-digital.com>
2020-12-10 19:52:06 +0100

    day 10, part 1

---
+++ src/main/kotlin/Day10.kt
@@ -0,0 +1,23 @@
+class Day10 {
+
+    fun part1(input: String): Int {
+        val differences = accumulateDifferences(readNumbers(input).withStart(0).withEnd(3))
+        return differences.getOrDefault(1, 0) * differences.getOrDefault(3, 0)
+    }
+
+    fun readNumbers(input: String) = input.trimIndent()
+            .split("\n")
+            .map { it.toInt() }
+
+    fun List<Int>.withStart(start: Int): List<Int> = this + start
+
+    fun List<Int>.withEnd(difference: Int): List<Int> = this + listOf((this.maxOrNull() ?: 0) + difference)
+
+    fun accumulateDifferences(numbers: List<Int>): MutableMap<Int, Int> =
+            numbers.sorted().windowed(2).fold(mutableMapOf()) { acc, window ->
+                val difference = window.last() - window.first()
+                acc[difference] = acc.getOrDefault(difference, 0) + 1
+                acc
+            }
+
+}
---
+++ src/test/groovy/Day10Spec.groovy
@@ -0,0 +1,63 @@
+import spock.lang.Specification
+import spock.lang.Subject
+
+class Day10Spec extends Specification {
+
+    def input = new File(getClass().getResource('/input/day10.txt').toURI()).getText()
+
+    @Subject
+    Day10 day10 = new Day10()
+
+    def part1() {
+        when:
+        def result = day10.part1(input)
+
+        then:
+        println("Part 1: $result")
+        result == 1885
+    }
+
+    def readNumbers() {
+        given:
+        def input = '''
+            35
+            20
+            15
+        '''
+
+        when:
+        def numbers = day10.readNumbers(input)
+
+        then:
+        numbers == [35, 20, 15]
+    }
+
+    def withStart() {
+        given:
+        def numbers = [16, 10]
+
+        expect:
+        day10.withStart(numbers, 0) == [16, 10, 0]
+    }
+
+    def withEnd() {
+        given:
+        def numbers = [16, 10]
+
+        expect:
+        day10.withEnd(numbers, 3) == [16, 10, 19]
+    }
+
+    def accumulateDifferences() {
+        given:
+        def numbers = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
+
+        when:
+        def differences = day10.accumulateDifferences(numbers)
+
+        then:
+        differences[1] == 6
+        differences[3] == 4
+    }
+
+}
