c60357e83a3f4bb99005a75e5a6097f7230b4602
Helge Wessels <helge.wessels@rewe-digital.com>
2020-12-16 18:44:37 +0100

    day 10 part 2

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -1,11 +1,17 @@
 package day10
 
+import kotlin.math.pow
+import kotlin.math.roundToLong
+
 class Day10 {
 
     fun part1(input: String): Int {
         val differences = accumulateDifferences(readNumbers(input).withStart(0).withEnd(3))
         return differences.getOrDefault(1, 0) * differences.getOrDefault(3, 0)
     }
+
+    fun part2(input: String): Long =
+            countDistinctValidArrangements(readNumbers(input).withStart(0).withEnd(3))
 
     fun readNumbers(input: String) = input.trimIndent()
             .split("\n")
@@ -22,4 +28,38 @@ class Day10 {
                 acc
             }
 
+    fun countDistinctValidArrangements(numbers: List<Int>): Long =
+            numbers.toDifferences()
+                    .split(3)
+                    .map { chunk -> numberOfOptions(chunk.size) }
+                    .reduce { acc, options -> acc * options }
+
+    fun List<Int>.toDifferences() =
+            sorted().windowed(2).map { window -> window.last() - window.first() }
+
+    fun List<Int>.split(delimiter: Int) =
+            joinToString("") { "$it" }
+                    .split(delimiter.toString())
+                    .map { chunk -> chunk.map { Character.getNumericValue(it) } }
+
+    fun numberOfOptions(size: Int): Long =
+            numberOfCombinations(size) - numberOfInvalidCombinations(size)
+
+    /**
+     * Counts all combinations with the constraint that the last has to be 1,
+     * because it's followed by a 3 (chunk in the middle) or it's the last one at all (last chunk).
+     */
+    private fun numberOfCombinations(size: Int): Long =
+            2.0.pow(size - 1).roundToLong()
+
+    /**
+     * Invalid combinations are 3 or more in a row, i.e. 000, 0000 etc.
+     * Combinations ending with 0 are not counted at all, so we can exclude the last invalid chunks here.
+     * E.g.
+     * - for 11111 there are 2 chunks of 000 (not counting the last), which is size - 3
+     * - for 11111 there is 1 chunk of 0000 (not counting the last), which is size - 4
+     */
+    private fun numberOfInvalidCombinations(size: Int): Long =
+            if (size < 3) 0
+            else (3 until size).sumBy { size - it }.toLong()
 }
--- src/test/groovy/day10/Day10Spec.groovy
+++ src/test/groovy/day10/Day10Spec.groovy
@@ -2,6 +2,7 @@ package day10
 
 import spock.lang.Specification
 import spock.lang.Subject
+import spock.lang.Unroll
 
 class Day10Spec extends Specification {
 
@@ -13,6 +14,11 @@ class Day10Spec extends Specification {
     def part1() {
         expect:
         day10.part1(input) == 1885
+    }
+
+    def part2() {
+        expect:
+        day10.part2(input) == 2024782584832
     }
 
     def readNumbers() {
@@ -56,6 +62,41 @@ class Day10Spec extends Specification {
         then:
         differences[1] == 6
         differences[3] == 4
+    }
+
+    @Unroll('#numbers has #distinctValidArrangements distinct valid arrangements')
+    def countDistinctValidArrangements(List<Integer> numbers, long distinctValidArrangements) {
+        expect:
+        day10.countDistinctValidArrangements(numbers) == distinctValidArrangements
+
+        where:
+        numbers                                                                                                                      | distinctValidArrangements
+        [0, 16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4, 22]                                                                               | 8
+        [0, 28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3, 52] | 19208
+    }
+
+    def 'numbers to differences'() {
+        expect:
+        day10.toDifferences([0, 16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4, 22]) == [1, 3, 1, 1, 1, 3, 1, 1, 3, 1, 3, 3]
+    }
+
+    def 'split numbers'() {
+        expect:
+        day10.split([1, 1, 3, 1, 3, 3, 1, 1, 1], 3) == [[1, 1], [1], [], [1, 1, 1]]
+    }
+
+    @Unroll('#ones has #options options')
+    def 'number of options in chunk of ones'(List<Integer> ones, long options) {
+        expect:
+        day10.numberOfOptions(ones.size()) == options
+
+        where:
+        ones            | options
+        [1]             | 1
+        [1, 1]          | 2
+        [1, 1, 1]       | 4
+        [1, 1, 1, 1]    | 7
+        [1, 1, 1, 1, 1] | 13
     }
 
 }
